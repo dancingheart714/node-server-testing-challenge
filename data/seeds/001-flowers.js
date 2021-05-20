2cd570b3ba9b9c69d749742924ca5b0e6e1957b1
Leah King <dancingheart714@yahoo.com>
2021-05-20 14:58:46 -0400

    seeds and cleaner done

--- data/seeds/001-flowers.js
+++ data/seeds/001-flowers.js
@@ -1,13 +1,14 @@
-
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
+exports.seed = function (knex) {
+  return knex('flowers')
+    .del()
     .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('flowers').insert([
+        { name: 'tulip' },
+        { name: 'rose' },
+        { name: 'daffodil' },
+        { name: 'crocus' },
+        { name: 'peonie' },
+        { name: 'daisy' },
       ]);
     });
 };
